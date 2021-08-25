e9aab5ba82d75368cb7456d70086dcf8f80418e3
Vaishali <vvyas@qa.com>
2021-08-25 12:53:52 +0100

    Maths commit

---
+++ src/main/java/com/natwest/JavaBeginners/Maths.java
@@ -0,0 +1,32 @@
+package com.natwest.JavaBeginners;
+
+public class Maths {
+
+	public static void main(String[] args) {
+	
+		double num1 = 37;
+		double num2 = 19;
+		
+		double result = num1+num2;
+		double multiply = num1*num2;
+		double subtract = num1-num2;
+		double divide = num1/num2;
+		
+		System.out.println(result);
+		System.out.println(multiply);
+		System.out.println(subtract);
+//		System.out.println(divide);
+	
+		if(num1 < num2) {
+			System.out.println(divide);}
+		else {
+			System.out.println("Division can not be performed");
+			
+		}
+	
+
+	
+	}
+	
+}
+
