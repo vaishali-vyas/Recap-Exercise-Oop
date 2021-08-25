184c2dd156474b736ccdcd06086fb875f6ab7ec8
Vaishali <vvyas@qa.com>
2021-08-25 11:29:09 +0100

    another helloworld commit

--- src/main/java/com/natwest/Helloworld/HelloWorld.java
+++ src/main/java/com/natwest/Helloworld/HelloWorld.java
@@ -2,9 +2,23 @@ package com.natwest.Helloworld;
 
 public class HelloWorld {
 
+	/* Create a method that accepts a String as a parameter, and then outputs it to
+	the console.*/
+	
+	public static void printMessage() {
+	     String message = "Hello World!";
+	     System.out.println(message);
+	 }
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		 printMessage();
+//		System.out.println("Hello World");
+
+//storing hello world in variable 
+
+//		String message = "Hello World";
+//		System.out.println(message);
+
 	}
 
 }
